63afc3ee607d20c60a41901b910c0c8b8471c80c
Vicente Junior <vicente_pc@outlook.com>
2025-08-11 18:19:29 -0300

    Implementado Testes para Selecionar/Editar Registros (ModuloDisciplina)

--- GeradorDeTestes.Testes.Integracao/ModuloDisciplina/RepositorioDisciplinaOrmTests.cs
+++ GeradorDeTestes.Testes.Integracao/ModuloDisciplina/RepositorioDisciplinaOrmTests.cs
@@ -3,8 +3,11 @@ using GeradorDeTestes.Infraestrutura.Orm.Compartilhado;
 using GeradorDeTestes.Infraestrutura.Orm.ModuloDisciplina;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace GeradorDeTestes.Testes.Integracao.ModuloDisciplina;
+
+//Padrao AAA 1-Arrange. 2-Action. 3.Assert)
 
 [TestClass]
 [TestCategory("Integração - Disciplina")]
@@ -13,8 +16,9 @@ public sealed class RepositorioDisciplinaOrmTests
     private GeradorDeTestesDbContext dbContext;
     private RepositorioDisciplinaOrm repositorioDisciplina;
 
-    [TestMethod]
-    public void CadastrarRegistroCorretamente()
+    //Método que é executado antes de cada teste.
+    [TestInitialize]
+    public void ConfigurarTestes()
     {
         //configurando qual projeto que será executado por meio do Assembly
         var assembly = typeof(RepositorioDisciplinaOrmTests).Assembly;
@@ -31,19 +35,68 @@ public sealed class RepositorioDisciplinaOrmTests
         dbContext = new GeradorDeTestesDbContext(options);
         repositorioDisciplina = new RepositorioDisciplinaOrm(dbContext);
 
-        //criando disciplina/Cadastrando/Selecionado por Id
+        dbContext.Database.EnsureDeleted(); // limpa o banco de dados para não haver conflitos entres testes
+        dbContext.Database.EnsureCreated();  //Cria banco de dados
+    }
+
+    [TestMethod]
+    public void CadastrarRegistroCorretamente()
+    {
+        //Ararnge
         var disciplina = new Disciplina("Programação Sequencial");
+
+        // Act
         repositorioDisciplina.Cadastrar(disciplina);
         dbContext.SaveChanges();
-        var registro = repositorioDisciplina.SelecionarRegistroPorId(disciplina.Id);
-        
-        //verifica se o objeto selecionado é igual ao cadastrado para teste
+
+        //Assert
+        var registro = repositorioDisciplina.SelecionarRegistroPorId(disciplina.Id);      
         Assert.AreEqual(disciplina, registro);
     }
 
     [TestMethod]
     public void SelecionarRegistrosCorretamente()
     {
+        //1 - Arrange.
+        var disciplina = new Disciplina("Programação Sequencial");
+        var disciplina1 = new Disciplina("Programação Extruturada");
+        var disciplina2 = new Disciplina("Programação Orientada a Objetos");
+        repositorioDisciplina.Cadastrar(disciplina);
+        repositorioDisciplina.Cadastrar(disciplina1);
+        repositorioDisciplina.Cadastrar(disciplina2);
 
+        dbContext.SaveChanges();
+
+        List<Disciplina> listDisciplinas = [disciplina ,disciplina1, disciplina2];
+
+        //2 - Act.
+        var registrosSelecionados = repositorioDisciplina.SelecionarRegistros();
+
+        //3.Assert
+        //Verifica se a lista dos selecionados é igual a lista que fois salva para o teste
+        CollectionAssert.Equals(listDisciplinas, registrosSelecionados); 
+    }
+
+    [TestMethod]
+    public void EditarRegistrosCorretamente()
+    {
+        //Arrange
+        var disciplina = new Disciplina("Vetores");
+        repositorioDisciplina.Cadastrar(disciplina);
+        dbContext.SaveChanges();
+
+        var disciplinaEditada = new Disciplina("Variaveis");
+
+        //Act
+        var etidadoComSucesso = repositorioDisciplina.Editar(disciplina.Id, disciplinaEditada);
+        dbContext.SaveChanges();
+
+        //Assert
+        var registroSelecionado = repositorioDisciplina.SelecionarRegistroPorId(disciplina.Id);
+
+        Assert.IsTrue(etidadoComSucesso); 
+            // verifica se o resultado da edição é true;
+        Assert.AreEqual(disciplina, registroSelecionado);
+            // verifica se os registros são iguais levando em contaa  referencia original dentro do contexto(método) do teste
     }
 }
