65fd7a981c6fd81110c7e4a4d9fb0bf5b7a7a89b
Vicente Junior <vicente_pc@outlook.com>
2025-08-19 14:07:26 -0300

    Adicionado Logs NewRelic

--- GeradorDeTestes.WebApp/DependencyInjection/SerilogConfig.cs
+++ GeradorDeTestes.WebApp/DependencyInjection/SerilogConfig.cs
@@ -9,11 +9,20 @@ public static class SerilogConfig
     {
         var caminhoAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         var caminhoArquivoLogs = Path.Combine(caminhoAppData, "GeradorDeTestes", "erro.log");
+        var licenseKey = configuration["NEWRELIC_LICENSE_KEY"];
+
+        if (string.IsNullOrWhiteSpace(licenseKey))
+            throw new Exception("A variável NEWRELIC_LICENSE_KEY não foi encontrada.");
 
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()
             .WriteTo.Console()
             .WriteTo.File(caminhoArquivoLogs, LogEventLevel.Error)
+            .WriteTo.NewRelicLogs(
+                endpointUrl: "http://log-api.newrelic.com/log/v1",
+                applicationName: "gerador-de-testes-app",
+                licenseKey: licenseKey
+            )
             .CreateLogger();
 
         logging.ClearProviders();
