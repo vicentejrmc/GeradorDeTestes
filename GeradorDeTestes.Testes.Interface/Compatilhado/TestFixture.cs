3b724f7bb8b9425efac8b245806ef22f7c47c57f
Vicente Junior <vicente_pc@outlook.com>
2025-08-21 13:51:28 -0300

    Implementação  inicial Testes de Interface. Implementado TestFixure

---
+++ GeradorDeTestes.Testes.Interface/Compatilhado/TestFixture.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+
+namespace GeradorDeTestes.Testes.Interface.Compatilhado;
+
+[TestClass]
+public abstract class TestFixture
+{
+    protected static IWebDriver? driver;
+
+    [TestInitialize]
+    public void ConfigureTest()
+    {
+        InitializeWebDriver();
+    }
+
+    [TestCleanup]
+    public void EncerrarTestes()
+    {
+        EncerrarWebDriver();
+    }
+
+    private static void InitializeWebDriver()
+    {
+        driver = new EdgeDriver();
+    }
+
+    private static void EncerrarWebDriver()
+    {
+        driver?.Quit();
+        driver?.Dispose();
+    }
+
+}
---
+++ GeradorDeTestes.Testes.Interface/ModuloDisciplina/DisciplinaInterfaceTests.cs
@@ -0,0 +1,13 @@
+﻿using GeradorDeTestes.Testes.Interface.Compatilhado;
+
+namespace GeradorDeTestes.Testes.Interface.ModuloDisciplina;
+[TestClass]
+[TestCategory("Interface - Disciplina - Testes")]
+public sealed class DisciplinaInterfaceTests : TestFixture
+{
+    [TestMethod]
+    public void Test()
+    {
+
+    }
+}
