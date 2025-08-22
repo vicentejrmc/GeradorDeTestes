b0235c5d903365094a374ba6804eab9effdc3e02
Vicente Junior <vicente_pc@outlook.com>
2025-08-22 14:30:35 -0300

    Implementações iniciais tetes Cadastrar Disciplina e editar / Cadastrar materia

--- GeradorDeTestes.Testes.Interface/ModuloDisciplina/DisciplinaInterfaceTests.cs
+++ GeradorDeTestes.Testes.Interface/ModuloDisciplina/DisciplinaInterfaceTests.cs
@@ -1,5 +1,6 @@
 ﻿using GeradorDeTestes.Testes.Interface.Compatilhado;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace GeradorDeTestes.Testes.Interface.ModuloDisciplina;
 [TestClass]
@@ -10,9 +11,9 @@ public sealed class DisciplinaInterfaceTests : TestFixture
     public void Deve_Cadastrar_Disciplina_Corretamente()
     {
         //Arrange
-        driver.Navigate().GoToUrl(Path.Combine(baseUrl, "disciplinas"));
+        driver?.Navigate().GoToUrl(Path.Combine(baseUrl, "disciplinas"));
 
-        var elemento = driver.FindElement(By.CssSelector("a[data-se='btnCadastrar']"));
+        var elemento = driver?.FindElement(By.CssSelector("a[data-se='btnCadastrar']"));
         elemento?.Click();
 
         //Act
@@ -22,5 +23,30 @@ public sealed class DisciplinaInterfaceTests : TestFixture
         //Assert
         var elementCard = driver?.FindElements(By.CssSelector(".card")); // Observer a diferentça para encontrar um elemento (FindElement), para uma coleção de elementos (FindElements)
         Assert.AreEqual(1, elementCard?.Count);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Disciplina_Corretamente()
+    {
+        //Arrange
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5)); // Espera até 5 segundos para que o elemento esteja presente na página para evitar falsos negativos
+
+        driver?.Navigate().GoToUrl(Path.Combine(baseUrl, "disciplinas"));
+        driver?.FindElement(By.CssSelector("a[data-se='btnCadastrar']")).Click();
+        driver?.FindElement(By.Id("Nome")).SendKeys("DisciplinaTeste");
+        driver?.FindElement(By.CssSelector("button[type='submit']")).Click();
+
+        wait.Until(d => d.FindElements(By.CssSelector(".card")).Count == 1);
+
+        driver?.FindElement(By.CssSelector(".card"))
+            .FindElement(By.CssSelector("a[title='Edição")).Click();
+
+        //Act
+        driver?.FindElement(By.Id("Nome")).SendKeys(" Editada");
+        driver?.FindElement(By.CssSelector("button[type='submit']")).Click();
+
+        //Assert
+        wait.Until(d => d.FindElements(By.CssSelector(".card")).Count == 1); // Espera até que haja apenas um card na página, indicando que a edição foi concluída para evitar falsos negativos
+        Assert.IsTrue(driver?.PageSource.Contains("DisciplinaTeste Editada"));
     }
 }
---
+++ GeradorDeTestes.Testes.Interface/ModuloMateria/MateriaInterfaceTests.cs
@@ -0,0 +1,37 @@
+﻿using GeradorDeTestes.Testes.Interface.Compatilhado;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V137.Audits;
+using OpenQA.Selenium.Support.UI;
+
+namespace GeradorDeTestes.Testes.Interface.ModuloMateria;
+
+[TestClass]
+[TestCategory("Interface - Materia - Testes")]
+public sealed class MateriaInterfaceTests : TestFixture
+{
+    [TestMethod]
+    public void Deve_Cadastrar_Materia_Corretamente()
+    {
+        //Arrange
+        //Cadastro de disciplina, necessario para o cadastro de materia(cadastro direto/resumido)
+        driver?.Navigate().GoToUrl(Path.Combine(baseUrl, "disciplinas", "Cadastrar"));     
+        driver?.FindElement(By.Id("Nome")).SendKeys("DisciplinaTeste");
+        driver?.FindElement(By.CssSelector("button[type='submit']")).Click();
+
+        driver?.Navigate().GoToUrl(Path.Combine(baseUrl, "materias", "Cadastrar"));
+
+        //Act
+        driver?.FindElement(By.Id("Nome")).SendKeys("MateriaTeste");
+        var selectSerie = new SelectElement(driver?.FindElement(By.Id("Serie"))!);  //SelectElement é uma classe do Selenium para manipular elementos <select> em formulários HTML(Pacote Externo/auxiliar ao selenium)
+        selectSerie.SelectByIndex(0);   
+        
+        var selectDisciplina = new SelectElement(driver?.FindElement(By.Id("DisciplinaId"))!);
+        selectDisciplina.SelectByText("DisciplinaTeste");
+        driver?.FindElement(By.CssSelector("button[type='submit']")).Click();
+
+        //Assert
+        var elementCard = driver?.FindElements(By.CssSelector(".card"));
+        Assert.AreEqual(1, elementCard?.Count);
+
+    }
+}
